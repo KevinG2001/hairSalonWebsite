0590cd078045d45286068ea1e3ef5eb4de748db1
Kevin <kevinglennon01@gmail.com>
2023-05-30 02:54:16 +0100

    Slideshow started but messed up navbar spacing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Slideshow from "./components/Slideshow";
 
 function App() {
   return (
     <>
       <div className="mainApp">
         <Navbar />
+        {/* <Slideshow /> */}
       </div>
     </>
   );
---
+++ src/components/Slideshow.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import slideStyles from "../styling/slideStyles.module.scss"
+
+function Slideshow() {
+  return (
+    <>
+    <div className={slideStyles.slideShowContainer}>
+        <div>Hi</div>
+    </div>
+    </>
+  )
+}
+
+export default Slideshow
