c8613181b2d1fb52a9279b95e76a93e74b76c629
Kevin <kevinglennon01@gmail.com>
2023-06-03 13:28:23 +0100

    Added more styling and last bits of information to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import footerStyles from "../styling/footerStyles.module.scss"
+import facebookImg from "../assets/socialImages/facebookImg.svg"
+import instagramImg from "../assets/socialImages/instagramImg.svg"
 
 function Footer() {
   return (
@@ -7,22 +9,43 @@ function Footer() {
         <div className={footerStyles.footerContainer}>
             <div className={footerStyles.footerContentWrapper}>
                 <div className={footerStyles.footerContentPanel}>
-                    <div>SHOP NOW</div>
-                    <div>haircare</div>
+                    <div className={footerStyles.footerTitle}>SHOP NOW</div>
+                    <div className={footerStyles.footerContent}>haircare</div>
                 </div>
                 <div className={footerStyles.footerContentPanel}>
-                    <div>LOGO</div>
-                    <div>Adress, Dublin what ever</div>
-                    <div>Number</div>
+                    <div className={footerStyles.footerTitle}>LOGO</div>
+                    <div className={footerStyles.footerContent}>15 Random Address</div>
+                    <div className={footerStyles.footerContent}>Dublin what ever</div>
+                    <div className={footerStyles.footerContent}>01 319 1412</div>
                 </div>
                 <div className={footerStyles.footerContentPanel}>
-                    <div>SITE INFO</div>
-                    <div>Home</div>
-                    <div>dermalogica</div>
-                    <div>delivery & returns policy</div>
+                    <div className={footerStyles.footerTitle}>SITE INFO</div>
+                    <div className={footerStyles.footerContent}>Home</div>
+                    <div className={footerStyles.footerContent}>dermalogica</div>
+                    <div className={footerStyles.footerContent}>delivery & returns policy</div>
                 </div>
             </div>
-            {/* NEED TO ADD 2 BUTTONS AND FOLLOW US */}
+            <div className={footerStyles.footerContentWrapper}>
+                <div className={footerStyles.footerContentPanel}>
+                    <button>
+                        Show all products
+                    </button>
+                </div>
+                <div className={footerStyles.footerContentPanel}>
+                    <div className={footerStyles.footerTitle}>
+                        Follow Us
+                    </div>
+                    <div className={footerStyles.footerSocialHolder}>
+                        <img src={facebookImg} alt="" className={footerStyles.footerImg}/>
+                        <img src={instagramImg} alt="" className={footerStyles.footerImg}/>
+                    </div>
+                </div>
+                <div className={footerStyles.footerContentPanel}>
+                    <button>
+                        book now
+                    </button>
+                </div>
+            </div>
         </div>
     </>
   )
