4dba395c0e3567a490ba991522de0b2458c13819
Kevin <kevinglennon01@gmail.com>
2023-06-14 12:50:32 +0100

    Added basic setup for card holders and card setup for new products (must add if added in last 6 months display here

--- src/components/Newproducts.tsx
+++ src/components/Newproducts.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import nProductStyles from "../styling/newproducts.module.scss";
+import productData from "../data/products.json";
 
-function Newproducts() {
-  return <div className={nProductStyles.nProductContainer}></div>;
-}
+const Newproducts = () => {
+  return (
+    <div className={nProductStyles.nProductContainer}>
+      <h1 className={nProductStyles.nProductContainerTitle}>NewProducts</h1>
+      <div className={nProductStyles.nProductCardHolder}>
+        {productData.products.map((product) => (
+          <div className={nProductStyles.nProductCard} key={product.id}>
+            <div className={nProductStyles.nProductTitle}>{product.name}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Newproducts;
