7280474fd489e206805c2da313e75e167c982d78
Kevin <kevinglennon01@gmail.com>
2023-06-14 11:58:05 +0100

    Added json file and new products tsx file

---
+++ src/components/Newproducts.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import nProductStyles from "../styling/newproducts.module.scss";
+
+function Newproducts() {
+  return <div className={nProductStyles.nProductContainer}></div>;
+}
+
+export default Newproducts;
