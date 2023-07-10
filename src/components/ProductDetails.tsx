650f80ebb5eac939a8839183b77f8b190cbd86bc
Kevin <kevinglennon01@gmail.com>
2023-07-10 20:00:04 +0100

    Fixed positioning

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -14,38 +14,40 @@ const ProductDetails = () => {
     <>
       <Navbar />
       <div className={productDetailStyling.detailWrapper}>
-        {product && (
-          <>
-            <img src={product.image} alt="" />
-            <div className={productDetailStyling.detailContainer}>
-              <div className={productDetailStyling.productHeading}>
-                <h2>{product.name}</h2>
-              </div>
-              <div className={productDetailStyling.productPrice}>
-                  € {product.price}
-              </div>
-              <div className={productDetailStyling.productCartOptions}>
-                <span className={productDetailStyling.productSpan}>Quantity:</span>
-                <div className={productDetailStyling.quantitySelector}>
-                  <button className={productDetailStyling.selectorBtn}>-</button>
-                  <button className={productDetailStyling.selectorBtn}>0</button>
-                  <button className={productDetailStyling.selectorBtn}>+</button>
-                  {product.quantity <= 0 ? (
-                    <p>The product is out of stock </p>
-                  ) : (
-                    <button className={productDetailStyling.addBtn}>Add to cart</button>
-                  )}
+        <div className={productDetailStyling.detailWrapper2}>
+          {product && (
+            <>
+              <img src={product.image} alt="" />
+              <div className={productDetailStyling.detailContainer}>
+                <div className={productDetailStyling.productHeading}>
+                  <h2>{product.name}</h2>
                 </div>
-    
-                <div className={productDetailStyling.descriptionBreaker}>
-                  <div className={productDetailStyling.Line}></div>
-                  <div className={productDetailStyling.descriptionTitle}>product description</div>
-                  <div className={productDetailStyling.Line}></div>
+                <div className={productDetailStyling.productPrice}>
+                    € {product.price}
+                </div>
+                <div className={productDetailStyling.productCartOptions}>
+                  <span className={productDetailStyling.productSpan}>Quantity:</span>
+                  <div className={productDetailStyling.quantitySelector}>
+                    <button className={productDetailStyling.selectorBtn}>-</button>
+                    <button className={productDetailStyling.selectorBtn}>0</button>
+                    <button className={productDetailStyling.selectorBtn}>+</button>
+                    {product.quantity <= 0 ? (
+                      <p>The product is out of stock </p>
+                    ) : (
+                      <button className={productDetailStyling.addBtn}>Add to cart</button>
+                    )}
+                  </div>
+      
+                  <div className={productDetailStyling.descriptionBreaker}>
+                    <div className={productDetailStyling.Line}></div>
+                    <div className={productDetailStyling.descriptionTitle}>product description</div>
+                    <div className={productDetailStyling.Line}></div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </>
-        )}
+            </>
+          )}
+          </div>
       </div>
     </>
   );
