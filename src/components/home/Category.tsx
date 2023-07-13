7136f352a7117e0e0937359190b5818d7692efc4
Kevin <kevinglennon01@gmail.com>
2023-07-13 17:26:23 +0100

    Category section has correct pictures

--- src/components/home/Category.tsx
+++ src/components/home/Category.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import catStyles from "../../styling/categoryStyles.module.scss";
+import haircare from "../../assets/category/haircare.webp"
+import brunettehair from "../../assets/category/brunettehair.webp"
+import dermotology from "../../assets/category/dermotology.webp"
+import giftset from "../../assets/category/giftset.webp"
+import blondehair from "../../assets/category/blondehair.webp"
 
 function Category() {
   return (
@@ -7,19 +12,24 @@ function Category() {
       <div className={catStyles.catContainer}>
         <div className={catStyles.cateBox}>
           <div className={catStyles.catOption}>
-            <button>Haircare</button>
+            <img src={haircare} alt="" className={catStyles.catImg}/>
+            <button >Haircare</button>
           </div>
-          <div className={catStyles.catOption}>
-            <button>Styling Product</button>
+          <div className={catStyles.catOption} id={catStyles.stylingProduct}>
+            <img src={brunettehair} alt="" className={catStyles.catImg}/>
+            <button >Styling Product</button>
           </div>
-          <div className={catStyles.catOption}>
-            <button>Beauty</button>
+          <div className={catStyles.catOption} id={catStyles.beauty}>
+            <img src={blondehair} alt="" className={catStyles.catImg}/>
+            <button >Beauty</button>
           </div>
-          <div className={catStyles.catOption}>
-            <button>Dermotology</button>
+          <div className={catStyles.catOption} id={catStyles.dermotology}>
+            <img src={dermotology} alt="" className={catStyles.catImg}/>
+            <button >Dermotology</button>
           </div>
-          <div className={catStyles.catOption}>
-            <button>Gift Set</button>
+          <div className={catStyles.catOption} id={catStyles.giftset}>
+            <img src={giftset} alt="" className={catStyles.catImg}/>
+            <button >Gift Set</button>
           </div>
         </div>
       </div>
