2244b2ba5b79bd862dcd404d04058200cd319c2a
Kevin <kevinglennon01@gmail.com>
2023-08-01 20:46:31 +0100

    Dynamically loading all categorys instead of having premade ones

--- src/components/home/Category.tsx
+++ src/components/home/Category.tsx
@@ -1,44 +1,31 @@
 import React from "react";
 import catStyles from "../../styling/categoryStyles.module.scss";
-import haircare from "../../assets/category/haircare.webp";
-import brunettehair from "../../assets/category/brunettehair.webp";
-import dermotology from "../../assets/category/dermotology.webp";
-import giftset from "../../assets/category/giftset.webp";
-import blondehair from "../../assets/category/blondehair.webp";
+import productData from "../../data/products.json";
 import { Link } from "react-router-dom";
+import { Product } from "../../types/producyTypes";
+
+function filterUniqueCategorys(products: Product[]): string[] {
+  const uniqueCategories = Array.from(new Set(products.map((product) => product.category)));
+  return uniqueCategories;
+}
+
 
 function Category() {
+  const products = productData.products;
+  const uniqueCategories = filterUniqueCategorys(products);
   return (
     <>
       <div className={catStyles.catContainer}>
         <div className={catStyles.cateBox}>
-          <Link to={"/category/haircare"}>
-            <div className={catStyles.catOption}>
-              <img src={haircare} alt="" className={catStyles.catImg} />
-              <button>Haircare</button>
-            </div>
-          </Link>
-          <Link to={"/category/styling-products"}>
-            <div className={catStyles.catOption} id={catStyles.stylingProduct}>
-              <img src={brunettehair} alt="" className={catStyles.catImg} />
-              <button>Styling Product</button>
-            </div>
-          </Link>
-          <div className={catStyles.catOption} id={catStyles.beauty}>
-            <img src={blondehair} alt="" className={catStyles.catImg} />
-            <button>Beauty</button>
-          </div>
-          <Link to={"/category/dermalogica"}>
-            <div className={catStyles.catOption} id={catStyles.dermotology}>
-              <img src={dermotology} alt="" className={catStyles.catImg} />
-              <button>Dermotology</button>
-            </div>
-          </Link>
-
-          <div className={catStyles.catOption} id={catStyles.giftset}>
-            <img src={giftset} alt="" className={catStyles.catImg} />
-            <button>Gift Set</button>
-          </div>
+          {uniqueCategories.map((category) => (
+            <>
+              <Link to={`/category/${category}`}>
+                <div className={catStyles.catOption} id={catStyles.giftset}>
+                  <button>{category}</button>
+                </div>
+              </Link>
+            </>
+          ))}
         </div>
       </div>
     </>
