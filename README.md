187bd43dd6758a9cfa96abc26b7ad6fe8b9628f3
Kevin <kevinglennon01@gmail.com>
2023-08-10 21:03:25 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-A hair salon website based off of the style from https://www.debonairhairandbeauty.ie/
-Made by Kevin Glennon
--- src/components/Products/StylingProduct.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function StylingProduct() {
-  return <div>Hi</div>;
-}
-
-export default StylingProduct;
