c88ca90cd8b9fbf60ec7f825a70cb719e3c6e31d
Kevin <kevinglennon01@gmail.com>
2023-09-10 17:49:46 +0100

    Navbar now works with burger menu

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -8,10 +8,10 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 function Navbar() {
-  const [isActive, setIsActive] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleBurgerMenu = () => {
-    setIsActive(!isActive);
+    setIsOpen(!isOpen);
   };
   return (
     <>
@@ -23,43 +23,46 @@ function Navbar() {
           <div className={navStyles.burgerMenu} onClick={toggleBurgerMenu}>
             <div
               className={`${navStyles.burgerLine} ${
-                isActive ? navStyles.activeLine1 : ""
+                isOpen ? navStyles.activeLine1 : ""
               }`}
-              id={navStyles.line1}
             ></div>
             <div
               className={`${navStyles.burgerLine} ${
-                isActive ? navStyles.activeLine2 : ""
+                isOpen ? navStyles.activeLine2 : ""
               }`}
-              id={navStyles.line2}
             ></div>
             <div
               className={`${navStyles.burgerLine} ${
-                isActive ? navStyles.activeLine3 : ""
+                isOpen ? navStyles.activeLine3 : ""
               }`}
-              id={navStyles.line3}
             ></div>
           </div>
         </div>
-        <div className={navStyles.navUlWrapper}>
-          <ul className={navStyles.navUl}>
-            <Link to="/" className={navStyles.navLink}>
-              Home
-            </Link>
-            <button className={navStyles.navLink}>Haircare</button>
-            <button className={navStyles.navLink}>Gift Vouchers</button>
-            <Link to="/services-price" className={navStyles.navLink}>
-              Price List
-            </Link>
-            <button className={navStyles.navLink}>Book Now</button>
-            <button className={navStyles.navLink}>Contact</button>
-          </ul>
-        </div>
-        <div className={navStyles.navSocials}>
-          <img src={facebookImg} alt="" className={navStyles.navSocailImg} />
-          <img src={instagramImg} alt="" className={navStyles.navSocailImg} />
-          <img src={searchImg} alt="" className={navStyles.navSocailImg} />
-          <img src={cartImg} alt="" className={navStyles.navSocailImg} />
+        <div
+          className={`${navStyles.dropdown} ${
+            isOpen ? navStyles.activeDropdown : ""
+          }`}
+        >
+          <div className={navStyles.navUlWrapper}>
+            <ul className={navStyles.navUl}>
+              <Link to="/" className={navStyles.navLink}>
+                Home
+              </Link>
+              <button className={navStyles.navLink}>Haircare</button>
+              <button className={navStyles.navLink}>Gift Vouchers</button>
+              <Link to="/services-price" className={navStyles.navLink}>
+                Price List
+              </Link>
+              <button className={navStyles.navLink}>Book Now</button>
+              <button className={navStyles.navLink}>Contact</button>
+            </ul>
+          </div>
+          <div className={navStyles.navSocials}>
+            <img src={facebookImg} alt="" className={navStyles.navSocailImg} />
+            <img src={instagramImg} alt="" className={navStyles.navSocailImg} />
+            <img src={searchImg} alt="" className={navStyles.navSocailImg} />
+            <img src={cartImg} alt="" className={navStyles.navSocailImg} />
+          </div>
         </div>
       </nav>
     </>
